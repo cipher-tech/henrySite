d41e359aa8122465a1f02ef60c82ab1125bef996
cipher-tech <nickchibuikem@gmail.com>
2021-02-10 22:47:32 +0100

    updated migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
+            $table->string('username');
             $table->string('email')->unique();
+            $table->string('phone_no');
+            $table->string('country');
+            $table->integer('wallet_balance')->default("0");
+            $table->integer('earnings')->default("0")->nullable();
+            $table->integer('current_plan')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
